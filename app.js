69d4565a56fb54b39cfa2541bdc42fac62331f6c
Tamir <tamir.segal1@mail.huji.ac.il>
2020-01-09 21:30:39 +0200

    added omorfi data handler

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const db = require("./db");
+const omorfiHandler = require("./omorfi_handler");
 
 const app = express();
 const port = 3000;
 
 app.get("/api/sana/:word", (req, res) => {
-  db.getWordData(req.params.word)
-    .then(wordData => res.send(wordData))
+  let word = req.params.word;
+
+  const omorfiWordData = omorfiHandler.getWordData(word);
+
+  // if omorfiWordData contains the word string, replace the word with it,
+  // since omorfi gives us the word in Nominative or Infinitive form (that's good!)
+  if (omorfiWordData.word !== null) {
+    word = omorfiWordData.word;
+  }
+
+  db.getWordData(word)
+    .then(wordData => {
+      wordData["omorfi"] = omorfiWordData;
+      res.send(wordData);
+    })
     .catch(e => res.send(e));
 });
 
--- omorfi_handler.js
+++ omorfi_handler.js
@@ -1,5 +1,6 @@
-var exec = require("child_process").exec;
+var child_process = require("child_process");
 
+const SCRIPT_PATH = "../omorfi/src/bash/omorfi-disambiguate-text.bash";
 const DEV = true;
 
 let cmd = "";
@@ -7,35 +8,110 @@ if (DEV) {
   cmd = "sh ";
 }
 
-// Verbs:
-// UPOS=VERB
-// Voice=Act
-// VerbForm=Inf
-// Person=3
-// Number=Sing/Plur
-// Case=Ill
-// Tense=Past/Present
-// Mood=Ind/Opt
-
-// Non-verbs:
-// UPOS=NOUN/PROPN/ADV/ADP/ADJ
-// Number=Sing/Plur
-// Case=Ill
-
-const formatOutput = stdout => {
-  let outputLines = stdout.split("\n").slice(3, -6);
-
-  outputLines = outputLines.map(line => line.trim());
-
-  let kaupunki = outputLines[3];
-
-  console.log(outputLines);
-  console.log(kaupunki.slice(" "));
+/**
+ * Maps the UPOS codes to a more readable string
+ * @param {string} uposCode
+ */
+const mapUposCodeToReadable = uposCode => {
+  switch (uposCode) {
+    case "VERB":
+      return "Verb";
+    case "PROPN":
+      return "Proper Noun";
+    case "ADV":
+      return "Adverb";
+    case "ADP":
+      return "Adposition";
+    case "ADJ":
+      return "Adjective";
+    case "AUX":
+      return "Auxiliary Verb";
+    case "CONJ":
+      return "Coordinating Conjunction";
+    case "DET":
+      return "Determiner";
+    case "SCONJ":
+      return "Subordinating Conjunction";
+    case "PUNCT":
+      return "Punctuation";
+    case "SYM":
+      return "Symbol";
+    default:
+      break;
+  }
 };
 
-exec(
-  `${cmd}../omorfi/src/bash/omorfi-disambiguate-text.bash`,
-  (error, stdout, stderr) => {
-    formatOutput(stdout);
-  }
-);
+/**
+ * formats the omorfi-disambiguate-text script
+ * stdout to a readable object
+ * @param {string} stdout
+ */
+const formatOutput = stdout => {
+  let outputLines = stdout.split("\n").slice(3, -6); // split and remove redundant rows
+  outputLines = outputLines.map(line => line.trim()); // remove edge whitespaces
+
+  // TODO handle all lines, not only 1
+  return lineToObject(outputLines[1]);
+};
+
+/**
+ * format a single omorfi-disambiguate-text script
+ * stdout line string to a readable object
+ *
+ * example for such line: "kaupunki" UPOS=NOUN Number=Sing Case=Nom <CMP=1> <W=0>
+ * @param {string} line
+ */
+const lineToObject = line => {
+  line = line.split(" "); // turn to array
+  line = line.slice(0, -2); // remove 'CMP' and 'W'
+
+  let lineObject = { word: null };
+
+  // parse array to object
+  line.map(e => {
+    let keyValArray = e.split("="); // example: e is "UPOS=VERB"
+
+    let key = keyValArray[0]; // example: key = "UPOS"
+    let value = keyValArray[1]; // example: value = "VERB"
+
+    if (value === undefined) {
+      // TODO handle case where word has # inside
+      lineObject["word"] = key; // value is undefined when key is the word itself
+    } else if (key === "UPOS") {
+      lineObject[key] = mapUposCodeToReadable(value);
+    } else {
+      lineObject[key] = value;
+    }
+  });
+
+  return lineObject;
+};
+
+/**
+ * returns data object in the following format:
+ * {
+ *  name: [the word, in NOM or INF],
+ *  UPOS: [VERB/NOUN/PROPN/...]
+ *  Case: [Nom/Gen/...],
+ *  Number: [Sing/Plur],
+ *  Person: [1/2/3]
+ *  Tense: [Present/Past]
+ *  Mood: [Ind/Opt/???]
+ * }
+ *
+ * @param {string} word
+ */
+const getWordData = word => {
+  // TODO handle stdin!!!!!
+
+  let result = child_process.execSync(`${cmd}${SCRIPT_PATH}`);
+  let stdinStream = child_process.stdin;
+
+  stdinStream.write(word);
+  stdinStream.end();
+  return formatOutput(result.toString());
+};
+
+module.exports = {
+  getWordData: getWordData
+};
